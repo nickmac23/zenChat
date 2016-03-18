030587a4710f160606beb83eab6741e187194558
nickmac <nickpmacintyre@gmail.com>
2016-03-18 16:12:40 -0600

    deploying to heroku

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/rooms/', function(req, res, next) {
+  // res.send(req.body.roomName.toUpperCase())
+  res.render('room', { strawberry: req.body.roomName.toUpperCase() })
+});
+
 module.exports = router;
